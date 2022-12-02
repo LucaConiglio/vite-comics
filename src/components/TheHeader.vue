e7989970b778f1b36c7883d86895ed10d7565b29
LucaConiglio <lucaconiglio.92@gmail.com>
2022-12-02 15:14:32 +0100

    inizio header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <div class="container m-auto">
+      <nav class="d-flex justify-content-between align-items-center">
+        <div>
+          <img src="./dc-logo.png" alt="">
+        </div>
+        <div class="d-flex gap-2">
+          <a href="#">CHARACTERS</a>
+          <a href="#">COMICS</a>
+          <a href="#">MOVIES</a>
+          <a href="#">TV</a>
+          <a href="#">GAMES</a>
+          <a href="#">COLLECTIBLES</a>
+          <a href="#">VIDEOS</a>
+          <a href="#">FANS</a>
+          <a href="#">NEWS</a>
+          <a href="#">SHOP</a>
+        </div>
+
+      </nav>
+    </div>
+  </header>
+    
+  
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
