abdde673d4882ee6bdbf7708ea5c385b9af1bc0b
LucaConiglio <lucaconiglio.92@gmail.com>
2022-12-02 18:09:38 +0100

    terminato statico da impl js in vari punti

--- src/components/SignUpFooter.vue
+++ src/components/SignUpFooter.vue
@@ -1,6 +1,23 @@
 <template>
   <section>
+    <div class="container">
+      <div class="d-flex justify-content-between align-items-center">
+        <div>
+          <button class="p-2">SIGN-UP NOW!</button>
+        </div>
+        <div class="d-flex gap-3 align-items-center">
+          <span>FOLLOW US</span>
+          <a href="#"><img src="footer-facebook.png" alt=""></a>
+          <a href="#"><img src="footer-twitter.png" alt=""></a>
+          <a href="#"><img src="footer-youtube.png" alt=""></a>
+          <a href="#"><img src="footer-pinterest.png" alt=""></a>
+          <a href="#"><img src="footer-periscope.png" alt=""></a>
+          
 
+        </div>
+      
+      </div>
+    </div>
   </section>
 </template>
 <script>
@@ -18,5 +35,16 @@ export default{
 section{
   padding: 2rem 0;
   background-color: #303030;
+  span{
+    color: #0676d6;
+    font-weight: 700;
+  }
+
+  button{
+    color: white;
+    font-weight: 600;
+    background-color: transparent;
+    border: 2px solid #0877de;
+  }
 }
 </style>
